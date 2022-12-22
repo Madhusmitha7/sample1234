0e7a96f05b95a1b18ef2a14c2e57d75eb33ca770
Madhusmitha7 <konda.madhusmitha@gmail.com>
2022-12-22 10:16:33 +0530

    My first commit

---
+++ Sampleproject123/src/main/java/Sampleproject123/Sampleproject123/App.java
@@ -0,0 +1,13 @@
+package Sampleproject123.Sampleproject123;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
